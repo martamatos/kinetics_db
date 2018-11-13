dbc2db91aae1b1ee458d5c297b54062ed253a06a
martamatos <marta.ra.matos@gmail.com>
2018-11-13 14:58:07 +0100

    First commit with tutorial app working locally

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    POSTGRES_URL = os.environ.get('POSTGRES_URL')
+    POSTGRES_USER = os.environ.get('POSTGRES_USER')
+    POSTGRES_PW = os.environ.get('POSTGRES_PW')
+    POSTGRES_DB = os.environ.get('POSTGRES_DB')
+
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 3
