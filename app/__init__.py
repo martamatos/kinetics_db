04d6cc74ab68140925cb5e4918d2b080d6537d47
martamatos <marta.ra.matos@gmail.com>
2019-03-12 17:21:16 +0100

    updated configs to use postgres for testing

--- app/__init__.py
+++ app/__init__.py
@@ -24,8 +24,6 @@ else:
         return isinstance(field, HiddenField)
 
 
-
-
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
@@ -45,7 +43,6 @@ def create_app(config_class=Config):
     mail.init_app(app)
     bootstrap.init_app(app)
 
-
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
@@ -54,6 +51,8 @@ def create_app(config_class=Config):
 
     from app.main import bp as main_np
     app.register_blueprint(main_np)
+
+    app.upload_path = app.config['UPLOAD_FOLDER']
 
     if not app.debug and not app.testing:
 
